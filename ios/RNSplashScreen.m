261284107cd5834275ac730f5366467386f8af25
JeongHun Kang <asatakjh@gmail.com>
2025-05-01 17:53:13 +0900

    fix: iOS perf monitor interaction (ExodusMovement#2)

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -151,7 +151,17 @@ NSString* RNSplashScreenOverlayName = @"splashscreenVideo";
   if (showingVideo) return;
   // let's try to hide, even if showing == false, ...just in case
 
-  UIImageView *imageView = (UIImageView *)[UIApplication.sharedApplication.keyWindow.subviews.lastObject viewWithTag:RNSplashScreenOverlayTag];
+  // UIImageView *imageView = (UIImageView *)[UIApplication.sharedApplication.keyWindow.subviews.lastObject viewWithTag:RNSplashScreenOverlayTag];
+  NSArray *subviews = UIApplication.sharedApplication.keyWindow.subviews;
+  UIImageView *imageView = (UIImageView *)[subviews.lastObject viewWithTag:RNSplashScreenOverlayTag];
+
+  #ifdef DEBUG
+  if (imageView == nil && subviews.count > 1) {
+    // Allows the SplashScreen to be removed even when the perf monitor is the last subview.
+    imageView = (UIImageView *)[subviews[subviews.count - 2] viewWithTag:RNSplashScreenOverlayTag];
+  }
+  #endif
+  
   if (imageView != nil) {
     [imageView removeFromSuperview];
   }
